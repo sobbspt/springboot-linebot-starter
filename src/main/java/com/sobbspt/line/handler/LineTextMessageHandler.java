2571ccd6ab1e9fedc59e1bcf0b048c4ae2361888
Wanichnun Sinpitak <kitaboons@gmail.com>
2018-08-30 13:40:46 +0700

    Init

---
+++ src/main/java/com/sobbspt/line/handler/LineTextMessageHandler.java
@@ -0,0 +1,16 @@
+package com.sobbspt.line.handler;
+
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@LineMessageHandler
+public class LineTextMessageHandler {
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        return new TextMessage(event.getMessage().getText());
+    }
+}
